9118a44f8a48eeec380651e86bebf80fb842f8f4
仝令辉 <tonglinghui@zuoyebang.com>
2020-10-12 19:55:36 +0800

    update:增加loader用法

--- src/search/index.js
+++ src/search/index.js
@@ -1,2 +1,15 @@
-let str = "大师级";
-document.write(str);
+'use strict'
+import React from 'react';
+import ReactDom from 'react-dom';
+import './index.scss'
+
+class Search extends React.Component{
+  render(){
+    return <div className="color">我要用react来做搜索</div>
+  }
+}
+
+ReactDom.render(
+  <Search />,
+  document.getElementById('root')
+)
--- webpack.config.js
+++ webpack.config.js
@@ -10,5 +10,28 @@ module.exports = {
     path: path.join(__dirname, "dist"),
     filename: "[name]-[hash].js",
   },
+  module:{
+    rules:[
+      {
+        test:/.js$/,
+        use:'babel-loader'
+      },
+      {
+        test:/\.css$/,
+        use:[
+          'style-loader', // 将js字符串生成为style节点（最后执行）
+          'css-loader', // 将css转成CommonJS模块（其次执行）
+        ]
+      },
+      {
+        test:/\.scss$/,
+        use:[
+          'style-loader', // 将js字符串生成为style节点（最后执行）
+          'css-loader', // 将css转成CommonJS模块（其次执行）
+          'sass-loader' // 将sass转成css（最先执行）
+        ]
+      }
+    ]
+  },
   mode: "production",
 };
